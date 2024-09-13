f39fe97d55da49641eadcfa52af92f3070a676a8
Manuel Bodlos <manuelbodlos@manuelbodlos.local>
2024-09-13 12:59:53 +0200

    Final commit

---
+++ link_mobility_test_project_project/src/main/java/at/semriach/link_mobility_test_project_project/LinkMobilityTestProjectProjectApplication.java
@@ -0,0 +1,15 @@
+package at.semriach.link_mobility_test_project_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkMobilityTestProjectProjectApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(LinkMobilityTestProjectProjectApplication.class, args);
+    }
+
+}
---
+++ link_mobility_test_project_project/src/test/java/at/semriach/link_mobility_test_project_project/LinkMobilityTestProjectProjectApplicationTests.java
@@ -0,0 +1,15 @@
+package at.semriach.link_mobility_test_project_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkMobilityTestProjectProjectApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
